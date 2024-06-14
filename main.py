46c7313381ad3e42a5bf748b4d06425485ccaa8c
EliasOMacias <167808478+EliasOMacias@users.noreply.github.com>
2024-06-14 10:34:15 -0700

    - first attempt at code, wrote to do list

---
+++ main.py
@@ -0,0 +1,18 @@
+
+#todo 1: pass complete names of four
+#todo 2: delete names longer than four
+#todo 3: create names of four from individual characters
+#todo 4: check for uniqueness and remove duplicates
+def friend(x)
+
+    real_friends = []
+    for name in x:
+        if len(name) == 4:
+            real_friends += name
+        else:
+            newName = ''
+            while len(newName) < 4:
+                for name in x:
+                    newName += name
+            real_friends += newName
+    return(real_friends)
