f477dbf5dc91d13fb33bdb0b5802ed62a9ea99b1
EliasOMacias <167808478+EliasOMacias@users.noreply.github.com>
2024-06-16 13:57:45 -0700

    - turned all inputs into one list

--- main.py
+++ main.py
@@ -1,22 +1,15 @@
 
-#todo 1: pass complete names of four
-#todo 2: delete names longer than four
-#todo 3: create names of four from individual characters
-#todo 4: check for uniqueness and remove duplicates
-def friend(x):
+#todo 1: change all inputs to one list
+#todo 2: pass names len = 4 to new list to be returned
+#todo 3: test
+def friend(*x):
 
-    real_friends = []
-    for name in x:
-        if len(name) == 4:
-            real_friends += name
-        else:
-            newName = ''
-            while len(newName) < 4:
-                for name in x:
-                    newName += name
-            real_friends += newName
-    return(real_friends)
+    joinedList = []
+    for list in x:
+        joinedList.extend(list)
+    print(joinedList)
 
 
+friend(['mark','david','john','jim'], ['steve','bob'])
     # added note alright
     # and another
